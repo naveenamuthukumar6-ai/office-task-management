f39a71b5bbcbcc1604ba12a6e34f212645b42d78
naveenamuthukumar6-ai <naveenamuthukumar6@gmail.com>
2026-01-02 17:02:37 +0530

    model register in admin

--- TaskManagement/accounts/admin.py
+++ TaskManagement/accounts/admin.py
@@ -3,3 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
 from .models import Role, UserProfile
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('role_code', 'name', 'level', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('role_code', 'name')
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'manager')
+    list_filter = ('role',)
+    search_fields = ('user__username', 'manager__username')
+    autocomplete_fields = ('user', 'manager')
+
+
