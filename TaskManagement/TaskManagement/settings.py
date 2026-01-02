e6f8f0cac9fe19e94b4921bcecb85e2a91bd13a5
naveenamuthukumar6-ai <naveenamuthukumar6@gmail.com>
2026-01-02 11:22:44 +0530

    Update DEBUG and ALLOWED_HOSTS for deployment

--- TaskManagement/TaskManagement/settings.py
+++ TaskManagement/TaskManagement/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
